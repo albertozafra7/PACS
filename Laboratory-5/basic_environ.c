3654e95457f58c2263af8ec3901690bbfd6fff29
Daniel Sanz <144448868+dsanz23@users.noreply.github.com>
2023-12-04 11:14:09 +0100

    holi

--- Laboratory-5/basic_environ.c
+++ Laboratory-5/basic_environ.c
@@ -201,16 +201,17 @@ int main(int argc, char** argv)
   err = clSetKernelArg(kernel, 2, sizeof(cl_uint), &arraySize);
   cl_error(err, "Failed to set argument 2\n");
 
+  printf("HOLI\n");
   // 9 Launch Kernel
   local_size = 128;
   global_size = 12800; //IDK which value(multiple of local size)
   err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
   cl_error(err, "Failed to launch kernel to the device\n");
-
+printf("HOLI\n");
   // 10 Read data form device memory back to host memory
   err = clEnqueueReadBuffer(command_queue, out_device_object, CL_TRUE, 0, sizeof(float) * arraySize, &out_device_object, 0, NULL, NULL);
   cl_error(err, "Failed to enqueue a read command\n");
-
+printf("HOLI\n");
   // 11 Write code to check correctness of execution ¿¿?? (idea chat gpt)
 
 
