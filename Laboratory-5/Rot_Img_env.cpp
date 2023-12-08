18fcef41cd8ac42f774c5a7cfbc5c1a1c88ada30
albertozafra7 <albertozafra7@gmail.com>
2023-12-08 01:09:40 +0100

    Rot_Img_env.cpp correct measurement and prints addition

--- Laboratory-5/Rot_Img_env.cpp
+++ Laboratory-5/Rot_Img_env.cpp
@@ -51,12 +51,20 @@ void initializeArray(float *array, size_t size) {
 int main(int argc, char** argv)
 {
 
-  // *** Global time ***
+  bool standard_print = true;
+  if(argc == 2){
+    if((strchr(argv[1], 'F') != NULL) || (strchr(argv[1], 'f') != NULL))
+      standard_print = false;
+  }
+
+  // -------- Global Execution time --------
 	clock_t global_start_time;
 
   global_start_time = clock();
 
-  // ********************
+  // --------------------------------------
+
+  // Start of the program
 
   int err;                            	// error code returned from api calls
   size_t t_buf = 50;			// size of str_buffer
@@ -83,68 +91,81 @@ int main(int argc, char** argv)
   // 1. Scan the available platforms:
   err = clGetPlatformIDs (num_platforms_ids, platforms_ids, &n_platforms);
   cl_error(err, "Error: Failed to Scan for Platforms IDs");
-  printf("Number of available platforms: %d\n\n", n_platforms);
+  if(standard_print)
+    printf("Number of available platforms: %d\n\n", n_platforms);
 
   for (int i = 0; i < n_platforms; i++ ){
     err= clGetPlatformInfo(platforms_ids[i], CL_PLATFORM_NAME, t_buf, str_buffer, &e_buf);
     cl_error (err, "Error: Failed to get info of the platform\n");
-    printf( "\t[%d]-Platform Name: %s\n", i, str_buffer);
+    if(standard_print)
+      printf( "\t[%d]-Platform Name: %s\n", i, str_buffer);
 
   // ***Task***: print on the screen the name, vendor, version, ...
 
     err= clGetPlatformInfo(platforms_ids[i], CL_PLATFORM_VENDOR, t_buf, str_buffer, &e_buf);
     cl_error (err, "Error: Failed to get info of the platform\n");
-    printf( "\t[%d]-Platform Vendor: %s\n", i, str_buffer);
+    if(standard_print)
+      printf( "\t[%d]-Platform Vendor: %s\n", i, str_buffer);
 
     err= clGetPlatformInfo(platforms_ids[i], CL_PLATFORM_VERSION, t_buf, str_buffer, &e_buf);
     cl_error (err, "Error: Failed to get info of the platform\n");
-    printf( "\t[%d]-Platform Version: %s\n", i, str_buffer);
+    if(standard_print)
+      printf( "\t[%d]-Platform Version: %s\n", i, str_buffer);
 
     
     err= clGetPlatformInfo(platforms_ids[i], CL_PLATFORM_PROFILE, t_buf, str_buffer, &e_buf);
     cl_error (err, "Error: Failed to get info of the platform\n");
-    printf( "\t[%d]-Platform Profile: %s\n", i, str_buffer);
+    if(standard_print)
+      printf( "\t[%d]-Platform Profile: %s\n", i, str_buffer);
 
   }
-  printf("\n");
+  if(standard_print)
+    printf("\n");
 	
   // 2. Scan for devices in each platform
   for (int i = 0; i < n_platforms; i++ ){
     err = clGetDeviceIDs(platforms_ids[i], CL_DEVICE_TYPE_ALL, num_devices_ids, devices_ids[i], &(n_devices[i]));
     cl_error(err, "Error: Failed to Scan for Devices IDs");
-    printf("\t[%d]-Platform. Number of available devices: %d\n", i, n_devices[i]);
+    if(standard_print)
+      printf("\t[%d]-Platform. Number of available devices: %d\n", i, n_devices[i]);
 
     for(int j = 0; j < n_devices[i]; j++){
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_NAME, sizeof(str_buffer), str_buffer, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device name");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_NAME: %s\n", i, j,str_buffer);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_NAME: %s\n", i, j,str_buffer);
 
       cl_uint max_compute_units_available;
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(max_compute_units_available), &max_compute_units_available, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device max compute units available");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_MAX_COMPUTE_UNITS: %d\n", i, j, max_compute_units_available);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_MAX_COMPUTE_UNITS: %d\n", i, j, max_compute_units_available);
 
       // ***Task***: print on the screen the cache size, global mem size, local memsize, max work group size, profiling timer resolution and ... of each device   
       cl_ulong DEVICE_GLOBAL_MEM_CACHE_SIZE;
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_GLOBAL_MEM_CACHE_SIZE, sizeof(DEVICE_GLOBAL_MEM_CACHE_SIZE), &DEVICE_GLOBAL_MEM_CACHE_SIZE, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device global memory cache size");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_GLOBAL_MEM_CACHE_SIZE: %ld\n", i, j,DEVICE_GLOBAL_MEM_CACHE_SIZE);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_GLOBAL_MEM_CACHE_SIZE: %ld\n", i, j,DEVICE_GLOBAL_MEM_CACHE_SIZE);
 
-	  cl_ulong DEVICE_LOCAL_MEM_SIZE;
+	    cl_ulong DEVICE_LOCAL_MEM_SIZE;
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_LOCAL_MEM_SIZE, sizeof(DEVICE_LOCAL_MEM_SIZE), &DEVICE_LOCAL_MEM_SIZE, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device local mem size");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_LOCAL_MEM_SIZE: %ld\n", i, j,DEVICE_LOCAL_MEM_SIZE);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_LOCAL_MEM_SIZE: %ld\n", i, j,DEVICE_LOCAL_MEM_SIZE);
 	  
 	  
-	  size_t DEVICE_MAX_WORK_GROUP_SIZE;
+	    size_t DEVICE_MAX_WORK_GROUP_SIZE;
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(DEVICE_MAX_WORK_GROUP_SIZE), &DEVICE_MAX_WORK_GROUP_SIZE, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device maximum work group size");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_MAX_WORK_GROUP_SIZE: %ld\n", i, j,DEVICE_MAX_WORK_GROUP_SIZE);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_MAX_WORK_GROUP_SIZE: %ld\n", i, j,DEVICE_MAX_WORK_GROUP_SIZE);
 
-	  size_t DEVICE_PROFILING_TIMER_RESOLUTION;
+	    size_t DEVICE_PROFILING_TIMER_RESOLUTION;
       err = clGetDeviceInfo(devices_ids[i][j], CL_DEVICE_PROFILING_TIMER_RESOLUTION, sizeof(DEVICE_PROFILING_TIMER_RESOLUTION), &DEVICE_PROFILING_TIMER_RESOLUTION, NULL);
       cl_error(err, "clGetDeviceInfo: Getting device profiling timer resolution");
-      printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_PROFILING_TIMER_RESOLUTION: %ld\n", i, j,DEVICE_PROFILING_TIMER_RESOLUTION);
+      if(standard_print)
+        printf("\t\t [%d]-Platform [%d]-Device CL_DEVICE_PROFILING_TIMER_RESOLUTION: %ld\n", i, j,DEVICE_PROFILING_TIMER_RESOLUTION);
     }
   }	
   
@@ -218,8 +239,16 @@ int main(int argc, char** argv)
   cl_mem out_device_object = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(cl_uchar)*outputImg.size(), NULL, &err);
   cl_error(err, "Failed to create memory buffer at device\n");
   
+
+  // -------- Memory Transfer time (data interchanged between host and device) --------
+
+  // Create events for measuring memory transfer time
+  cl_event writeEvent, readEvent;
+
+  // -------- Global WRITE bandwithd --------
+
   // 7 Write date into the memory object 
-  err = clEnqueueWriteBuffer(command_queue, in_device_object, CL_TRUE, 0, sizeof(cl_uchar) * inputImg.size(), inputImg.data(), 0, NULL, NULL);
+  err = clEnqueueWriteBuffer(command_queue, in_device_object, CL_TRUE, 0, sizeof(cl_uchar) * inputImg.size(), inputImg.data(), 0, NULL, &writeEvent);
   cl_error(err, "Failed to enqueue a write command\n");
   
   // 8 Set the arguments to the kernel
@@ -247,16 +276,39 @@ int main(int argc, char** argv)
   global_size[0] = img_width; // N of work items que quieres
   global_size[1] = img_height; // N of work items que quieres
 
-  err = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_size, NULL/*local_size*/, 0, NULL, NULL);
+
+  // -------- Kernel execution time --------
+  cl_event Kernel_exectime_event;
+
+  err = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_size, NULL/*local_size*/, 0, NULL, &Kernel_exectime_event);
   cl_error(err, "Failed to launch kernel to the device\n");
 
+  // -------- Kernel device bandwithd --------
+  // Create an event for measuring kernel execution time
+  cl_event kernel_local_bandwidth_event;
+
+  clFinish(command_queue); // Make sure previous commands are finished before recording the kernel event
+  err = clEnqueueMarkerWithWaitList(command_queue, 0, NULL, &kernel_local_bandwidth_event);
+  cl_error(err, "Failed to enqueue marker for kernel event\n");
+
+  clWaitForEvents(1, &Kernel_exectime_event);
+  clFinish(command_queue);
+
+  // -------- Global READ bandwithd --------
+
   // 10 Read data form device memory back to host memory
-  err = clEnqueueReadBuffer(command_queue, out_device_object, CL_TRUE, 0, sizeof(cl_uchar)*outputImg.size(), outputImg.data(), 0, NULL, NULL);
+  err = clEnqueueReadBuffer(command_queue, out_device_object, CL_TRUE, 0, sizeof(cl_uchar)*outputImg.size(), outputImg.data(), 0, NULL, &readEvent);
   cl_error(err, "Failed to enqueue a read command\n");
 
+  // Wait for the commands to finish --> bandwidth
+  clFinish(command_queue);
+
+
   // 11 Write code to check correctness of execution
-  inputImg.display("My first CImg code");  
-  outputImg.display("Rotated IMG");
+  if(standard_print){
+    inputImg.display("My first CImg code");  
+    outputImg.display("Rotated IMG");
+  }
 
   // 12 Release all the OpenCL memory objects allocated along the program
   clReleaseMemObject(in_device_object);
@@ -266,10 +318,79 @@ int main(int argc, char** argv)
   clReleaseCommandQueue(command_queue);
   clReleaseContext(context);
 
-// **** Total execution time in seconds ****
-  clock_t exec_time = clock() - global_start_time;
-  std::cout << "Total execution time = " << ((float)exec_time)/CLOCKS_PER_SEC  << " seconds" << std::endl;
+ // **************** Measurement calculations ****************
 
+  // +++++ Total execution time in seconds +++++
+
+  clock_t exec_time = clock() - global_start_time;
+  if(standard_print)
+    std::cout << "Total execution time = " << ((float)exec_time)/CLOCKS_PER_SEC  << " seconds" << std::endl;
+
+
+  // +++++ Kernel Execution Time +++++
+
+  cl_ulong kernel_time_start;
+  cl_ulong kernel_time_end;
+  // Get starting and ending time of the event
+  clGetEventProfilingInfo(Kernel_exectime_event, CL_PROFILING_COMMAND_START, sizeof(kernel_time_start), &kernel_time_start, NULL);
+  clGetEventProfilingInfo(Kernel_exectime_event, CL_PROFILING_COMMAND_END, sizeof(kernel_time_end), &kernel_time_end, NULL);
+  double kernel_exec_time_ns = kernel_time_end-kernel_time_start;   // Get the execution time of the kernel in nanoseconds
+  //printf("Kernel Execution time: %0.3f milliseconds \n",kernel_exec_time_ns / 1000000.0);
+  if(standard_print)
+    printf("Kernel Execution time %0.10f seconds \n", kernel_exec_time_ns / 1.0e+9);  // Print the execution time in seconds
+
+
+  // +++++ Bandwidth --> HOST TO DEVICE +++++
+
+  // Calculate the time taken for write and read operations
+  cl_ulong writeStart, writeEnd, readStart, readEnd;
+  clGetEventProfilingInfo(writeEvent, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &writeStart, NULL);
+  clGetEventProfilingInfo(writeEvent, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &writeEnd, NULL);
+  clGetEventProfilingInfo(readEvent, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &readStart, NULL);
+  clGetEventProfilingInfo(readEvent, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &readEnd, NULL);
+
+  // Calculate bandwidth
+  double writeTime = (writeEnd - writeStart) * 1.0e-9; // Convert nanoseconds to seconds
+  double readTime = (readEnd - readStart) * 1.0e-9;
+  size_t dataSize = sizeof(cl_uchar) * inputImg.size();
+
+  double writeBandwidth = dataSize / writeTime; // in bytes per second
+  double readBandwidth = dataSize / readTime;
+
+  // Print or use the bandwidth values as needed
+  if(standard_print){
+    printf("General Write Bandwidth (Host to device): %.2f MB/s\n", writeBandwidth / (1024 * 1024));
+    printf("General Read Bandwidth (Host to device): %.2f MB/s\n", readBandwidth / (1024 * 1024));
+  }
+
+
+  // +++++ Bandwidth --> DEVICE TO LOCAL MEMORY +++++
+
+  cl_ulong kernelStart, kernelEnd;
+  clGetEventProfilingInfo(kernel_local_bandwidth_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &kernelStart, NULL);
+  clGetEventProfilingInfo(kernel_local_bandwidth_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &kernelEnd, NULL);
+
+  // Calculate bandwidth
+  double kernelTime = (kernelEnd - kernelStart); // Convert nanoseconds to miliseconds
+  size_t dataSize_kernel = sizeof(cl_uchar) * inputImg.size(); // Adjust data size based on your specific kernel data requirements
+  double kernelBandwidth = dataSize_kernel / kernelTime; // in bytes per nanosecond
+  
+
+  // Print or use the bandwidth value as needed
+  if(standard_print)
+    printf("Kernel Bandwidth (Device access to local memory): %.4f MB/ns\n", kernelBandwidth / (1024 * 1024));
+
+  // **************** Measurement prints for further analyzing ****************
+  // The output will be reduced to the following prints:
+  // total execution time, kernel execution time, Host to device (Write) bandwidth, Host to device (Read) Bandwidth, Kernel bandwidth
+  if(!standard_print)
+    printf("%.10f, %.10f, %.10f, %.10f, %.10f\n", 
+          ((float)exec_time)/CLOCKS_PER_SEC, 
+          (kernel_exec_time_ns / 1.0e+9), 
+          writeBandwidth / (1024 * 1024), 
+          readBandwidth / (1024 * 1024), 
+          kernelBandwidth / (1024 * 1024));
+  
 
   return 0;
 }
