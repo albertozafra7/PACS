3d004c61ae99ec4fe37756ac93c6170370eafa8a
Daniel Sanz <144448868+dsanz23@users.noreply.github.com>
2024-01-04 13:46:16 +0100

    cl_false

--- Laboratory-6/Flip_Img_env.cpp
+++ Laboratory-6/Flip_Img_env.cpp
@@ -360,7 +360,7 @@ int main(int argc, char** argv)
   // 10 Read data from device memory back to host memory
   for (size_t i = 0; i < n_images; ++i) {
       for (size_t dev = 0; dev < 2; ++dev) {
-          err = clEnqueueReadBuffer(command_queue[dev], out_device_object[dev], CL_TRUE, 0, sizeof(cl_uchar3) * (img_width * img_height), outputImg, 0, NULL, &readEvent[dev][i]);
+          err = clEnqueueReadBuffer(command_queue[dev], out_device_object[dev], CL_FALSE, 0, sizeof(cl_uchar3) * (img_width * img_height), outputImg, 0, NULL, &readEvent[dev][i]);
           cl_error(err, "Failed to enqueue a read command\n");
       }
   }
